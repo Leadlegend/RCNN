ed53b9b2ebef943b8c3fa55b0e89e25ad714018c
Leadlegend <55452442+Leadlegend@users.noreply.github.com>
2022-06-05 16:50:04 +0800

    add loss for cnn backend and regression model

--- model/criterions.py
+++ model/criterions.py
@@ -2,6 +2,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+criterion = nn.CrossEntropyLoss()
+if torch.cuda.is_available():
+    criterion.cuda()
 
-def Loss(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
-    pass
+
+def CNNLoss(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+    loss = criterion(y_pred, y_true)
+    return loss
+
+
+def RegLoss(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+    no_object_loss = torch.pow((1 - y_true[:, 0]) * y_pred[:, 0], 2).mean()
+    object_loss = torch.pow((y_true[:, 0]) * (y_pred[:, 0] - 1), 2).mean()
+
+    reg_loss = (
+        y_true[:, 0] * (torch.pow(y_true[:, 1:5] - y_pred[:, 1:5], 2).sum(1))).mean()
+
+    loss = no_object_loss + object_loss + reg_loss
+    return loss
