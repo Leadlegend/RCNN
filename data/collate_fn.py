8dfa6f40a3f090985618485f439d9bfcd6b1ed50
Leadlegend <55452442+Leadlegend@users.noreply.github.com>
2022-06-05 16:53:24 +0800

    add 3 collate_fn func

--- data/collate_fn.py
+++ data/collate_fn.py
@@ -1,25 +1,72 @@
 import torch
+import numpy as np
 
-from .dataset import Batch
+from typing import Optional
+from dataclasses import dataclass
 
 
-def collate_fn(batch, labeled: bool):
-    cell_ids, drug_ids = list(), list()
+@dataclass
+class ImgBatch:
+    datas: torch.Tensor
+    labels: Optional[torch.Tensor] = None
+
+    def __getitem__(self, idx: int):
+        if idx:
+            return self.labels
+        else:
+            return self.datas
+
+    def to(self, device):
+        if self.labels is not None:
+            return (self.datas.to(device), self.labels.to(device))
+        else:
+            return (self.datas.to(device), None)
+
+
+def alex_collate_fn(batch, labeled: bool = True):
     if labeled:
-        labels = list()
-        for data in batch:
-            cell_id, drug_id, label = data.cell_id, data.drug_id, data.label
-            cell_ids.append(cell_id)
-            drug_ids.append(drug_id)
-            labels.append(label)
-        cell_ids, drug_ids, labels = torch.Tensor(cell_ids).int(
-        ), torch.Tensor(drug_ids).int(), torch.Tensor(labels)
-        return Batch(cell_ids, drug_ids, labels.unsqueeze_(-1))
+        imgs, labels = trivial_collate_fn(batch)
+        imgs, labels = torch.Tensor(imgs, dtype=float32), torch.Tensor(labels).int()
+        return ImgBatch(imgs, labels)
     else:
-        for data in batch:
-            cell_id, drug_id = data.cell_id, data.drug_id
-            cell_ids.append(cell_id)
-            drug_ids.append(drug_id)
-        cell_ids, drug_ids = torch.Tensor(
-            cell_ids).int(), torch.Tensor(drug_ids).int()
-        return Batch(cell_ids, drug_ids)
+        imgs = trivial_collate_fn(batch)
+        if isinstance(imgs, tuple):
+            imgs = imgs[0]
+        imgs = torch.Tensor(imgs, dtype=float32)
+        return ImgBatch(imgs, None)
+
+
+def ft_collate_fn(batch):
+    return alex_collate_fn(batch, labeled=True)
+
+def reg_collate_fn(batch):
+    _, features, labels = trivial_collate_fn(batch)
+    features, labels = torch.Tensor(
+        features, dtype=float32), torch.Tensor(labels).int()
+    return d.ImgBatch(features, labels)
+
+
+def trivial_collate_fn(batch):
+    """
+    Params:
+        batch: List[Data]
+    Return:
+        mini_batch: List[Batch_Item]
+        where Batch_Item is List[Data_Item]
+    You should ensure that all the data in the batch of the same format
+    which means that you can't collate mixed labeled/unlabeled data with this collate_fn
+    """
+    mini_batch = list()
+    batch_size = len(batch)
+    sdata = batch[0]
+    for idx in range(len(sdata)):
+        data_item = sdata[idx]
+        data_size = [batch_size] + list(data_item.shape)
+        batch_item = np.zeros(data_size)
+        mini_batch.append(batch_item)
+
+    for idx, data in enumerate(batch):
+        for didx in range(len(data)):
+            mini_batch[didx][idx] = data[didx]
+
+    return tuple(mini_batch)
