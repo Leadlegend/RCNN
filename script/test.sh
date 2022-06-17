f9b3d0c096cbcf9703fbc34c5a16cbf7b876df65
kay <kaixinzhang@pku.edu.cn>
2022-06-17 21:54:26 +0800

    update test script

--- script/test.sh
+++ script/test.sh
@@ -1,2 +1,2 @@
 #export PYTHONWARNINGS='ignore:resource_tracker:UserWarning'
-python train_step2.py hydra.run.dir=./outputs/train_step2
+python test.py hydra.run.dir=./outputs/test data.test.batch_size=1
