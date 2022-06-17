c53c2fa8c2f22916916f39e57e0cc3eab46fb2eb
iris <kaixinzhang@pku.edu.cn>
2022-06-17 13:16:45 -0700

    update scripts

--- script/test.sh
+++ script/test.sh
@@ -1,2 +1,4 @@
 #export PYTHONWARNINGS='ignore:resource_tracker:UserWarning'
 nohup python test.py hydra.run.dir=./outputs/test data.test.batch_size=1 > ./outputs/test/test.log &
+cd test
+python pascalvoc.py
